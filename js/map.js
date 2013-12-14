8efd14d1c1a4ac080c1eebdee0d8d70ba64035dc
nathanallen <thenathanator@gmail.com>
2013-12-14 14:09:06 -0800

    finds places using midpoint

--- js/map.js
+++ js/map.js
@@ -1,50 +1,68 @@
 $(document).ready(function(){
   initialize()
   findLatLong('717 california st, san francisco')
-  findLatLong('Pacifica, california')
+  findLatLong('Mission, san francisco')
 })
 
-  var geocoder = new google.maps.Geocoder()
-  var map;
-  var centerpoint = {
-    points: []
+var geocoder = new google.maps.Geocoder()
+var map;
+var centerpoint = {
+  points: []
+};
+
+function initialize() {
+  var mapOptions = {
+    center: new google.maps.LatLng(37.7833, -122.4167),
+    zoom: 12
   };
+  map = new google.maps.Map(document.getElementById("map-canvas"),
+      mapOptions);
+}
 
-  function initialize() {
-    var mapOptions = {
-      center: new google.maps.LatLng(37.7833, -122.4167),
-      zoom: 12
-    };
-    map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
-  }
-
-  function findLatLong(address){
-    geocoder.geocode( { 'address': address }, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        var latLng = results[0].geometry.location
-        centerpoint.points.push(latLng)
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        })
-        if (centerpoint.points.length == 2){
-          var midPoint = findTheMiddle()
-          map.setCenter(new google.maps.LatLng(midPoint.lat, midPoint.lng))
-        }
-      } else {
-        alert("Geocode was not successful for the following reason: " + status);
+function findLatLong(address){
+  geocoder.geocode( { 'address': address }, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var latLng = results[0].geometry.location
+      centerpoint.points.push(latLng)
+      var marker = new google.maps.Marker({
+          map: map,
+          position: latLng
+      })
+      if (centerpoint.points.length == 2){
+        populateTheMiddle()
       }
-    })
-  }
-
-  function findTheMiddle(){
-    return {
-      lat: (centerpoint.points[0].nb + centerpoint.points[1].nb)/2,
-      lng: (centerpoint.points[0].ob + centerpoint.points[1].ob)/2
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
     }
+  })
+}
+
+function findTheMiddle(){
+  return {
+    lat: (centerpoint.points[0].nb + centerpoint.points[1].nb)/2,
+    lng: (centerpoint.points[0].ob + centerpoint.points[1].ob)/2
+  }
+}
+
+function populateTheMiddle(types){
+  var types = types || ['cafe']
+  var midPoint = findTheMiddle()
+  var middle = new google.maps.LatLng(midPoint.lat, midPoint.lng)
+  map.setCenter(middle)
+
+  var requestOptions = {
+    location: middle,
+    radius: '500',
+    types: types
   }
 
+  var service = new google.maps.places.PlacesService(map)
+  service.nearbySearch(requestOptions, createMarkers)
+}
 
-  //map.setCenter(latLng?)
-  //$.each(centerpoint.points, function(i,latlong){})
+function createMarkers(results, status) {
+  if (status == "ZERO_RESULTS"){
+    alert("no results found")
+  }
+  console.log(results)
+}
