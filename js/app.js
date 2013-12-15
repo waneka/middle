eb9fa68c9dd72da654e20908494385f69537e7b1
nathanallen <thenathanator@gmail.com>
2013-12-14 16:07:32 -0800

    the last of the glue: reading location from url

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ var App = {
   init: function(gmap){
     this.gmap = gmap
     this.gmap.initialize()
-    this.gmap.findLatLong('717 california st, san francisco')
-    this.gmap.findLatLong('Mission, san francisco')
+    var location1 = findAddress1() || '717 california st, san francisco'
+    var location2 = findAddress2() || 'Mission, san francisco'
+    this.gmap.findLatLong(location1)
+    this.gmap.findLatLong(location2)
   }
 }
