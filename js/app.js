eff36f43874e466aa74096607da0daf967579ca3
nathanallen <thenathanator@gmail.com>
2013-12-16 16:18:03 -0800

    initialize with App.user data

--- js/app.js
+++ js/app.js
@@ -7,10 +7,8 @@ var App = {
   init: function(gmap){
     this.gmap = gmap
     this.gmap.initialize()
-    var location1 = findAddress1() || '717 california st, san francisco'
-    var location2 = findAddress2() || 'Mission, san francisco'
-    this.gmap.findLatLong(location1)
-    this.gmap.findLatLong(location2)
+    this.gmap.findLatLong(this.user.address[1])
+    this.gmap.findLatLong(this.user.address[2])
   }
 }
 
