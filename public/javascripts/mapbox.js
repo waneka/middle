4540e30f351f57603bd429dbebfff72fe9cc1fc9
Tyler <twwaneka@gmail.com>
2014-06-02 18:26:17 -0700

    GOD COMMIT
    - Completely new UI for site
    - No lie, I didn't keep up with Git during this process. I'll do better, I swear.

--- public/javascripts/mapbox.js
+++ public/javascripts/mapbox.js
@@ -1,36 +1,92 @@
 var Map = {
   startingPoints: [],
   locationTypes: [],
+  venueInfo: {},
   counter: 0,
   initLoad: false,
-  updateFlag: false,
 
-  init: function(callback) {
-    this.map = L.mapbox.map('map', 'waneka.i5nnfp13').on('viewreset', function() {
-      callback()
-    })
+  init: function() {
+    this.map = L.mapbox.map('map', 'waneka.i5nnfp13', {zoomControl: false})
+    new L.Control.Zoom({ position: 'topright' }).addTo(this.map)
     this.venueLayer = L.mapbox.featureLayer().addTo(this.map)
     this.humanLayer = L.mapbox.featureLayer().addTo(this.map)
     this.userOneDirectionsLayer = L.mapbox.featureLayer().addTo(this.map)
     this.userTwoDirectionsLayer = L.mapbox.featureLayer().addTo(this.map)
   },
 
-  findLocation: function(address, email) {
+  geoLocation: function() {
+    View.initiateSpinner()
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(Map.initialGeoLocation)
+    } else {
+      View.geoLocationError()
+    }
+  },
+
+  initialGeoLocation: function(position) {
+    View.overlay()
+    var location = {
+      location: [ position.coords.longitude, position.coords.latitude ]
+    }
+    Map.startingPoints.push(location)
+    Map.setStartingMarkers()
+    Map.map.setView([position.coords.latitude, position.coords.longitude], 14)
+    View.showLocationsWindow()
+  },
+
+  initialManualLocation: function(address) {
+    View.overlay()
+    View.showLocationsWindow()
+    this.findLocation(address, function(location) {
+      var reverseCoords = [location[1],location[0]]
+      Map.setStartingMarkers()
+      Map.map.setView(reverseCoords, 14)
+    })
+  },
+
+  setStartingMarkers: function() {
+    var humanLocations = []
+    var counter = 0
+    this.startingPoints.forEach(function(point) {
+      var color
+      if (counter === 0) {
+        color = "#16a085"
+      } else {
+        color = "#c0392b"
+      }
+      var geoJSON = {
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": point.location
+          },
+          "properties": {
+            "title": "Human",
+            "marker-color": color,
+            "marker-size": "large",
+            "marker-symbol": "star-stroked"
+          }
+        }
+      humanLocations.push(geoJSON)
+      counter++
+    })
+    this.humanLayer.setGeoJSON(humanLocations)
+  },
+
+  findLocation: function(address, callback) {
     var self = this
-    self.startingPoints = []
     var geocoder = L.mapbox.geocoder('waneka.i5nnfp13')
     geocoder.query(address, function(err, result) {
       var location = {
-        location: result.latlng,
-        email: email
+        location: [result.latlng[1],result.latlng[0]],
       }
       self.startingPoints.push(location)
-      self.recenterMap()
+      callback(location.location)
     })
   },
 
   recenterMap: function() {
-    if (this.startingPoints.length === 2) {
+    if (this.startingPoints.length >= 2) {
       this.middle = this.findTheMiddle()
       this.setStartingMarkers()
       this.fetchVenueResults()
@@ -41,27 +97,6 @@ var Map = {
 
   fitBounds: function() {
     this.map.fitBounds(this.humanLayer.getBounds())
-  },
-
-  setStartingMarkers: function() {
-    var humanLocations = []
-    this.startingPoints.forEach(function(point) {
-      var geoJSON = {
-          "type": "Feature",
-          "geometry": {
-            "type": "Point",
-            "coordinates": [point.location[1],point.location[0]]
-          },
-          "properties": {
-            "title": point.email,
-            "marker-color": "#fc4353",
-            "marker-size": "large",
-            "marker-symbol": "star-stroked"
-          }
-        }
-      humanLocations.push(geoJSON)
-    })
-    this.humanLayer.setGeoJSON(humanLocations)
   },
 
   callback: function() {
@@ -76,96 +111,6 @@ var Map = {
     this.fetchVenues('coffee', this.callback)
     this.fetchVenues('food', this.callback)
     this.fetchVenues('drinks', this.callback)
-  },
-
-  populateTheMiddle: function() {
-    if (this.initLoad === true) {
-      var geoLocations = []
-      this.locationTypes.forEach(function(type) {
-        geoLocations.push(Map.addMarkers(type))
-      })
-      var merged = []
-      merged = merged.concat.apply(merged, geoLocations)
-      this.venueLayer.setGeoJSON(merged)
-      this.bindDirectionsEvent(this.userOneDirectionsLayer, 0, Map.venueLayer)
-      this.bindDirectionsEvent(this.userTwoDirectionsLayer, 1, Map.venueLayer)
-    }
-  },
-
-  bindDirectionsEvent: function(dirLayer, human, layer) {
-    layer.on('click', function(e) {
-      $.ajax({
-        type: 'POST',
-        url: '/directions',
-        data: {
-          pointOne: Map.startingPoints[human].location,
-          pointTwo: e.layer.feature.geometry.coordinates
-        },
-        dataType: 'json'
-      }).success(function(data) {
-        var geoArray = data.routes[0].geometry.coordinates
-        Map.drawRoute(geoArray, dirLayer)
-
-        var steps = data.routes[0].steps
-        View.displaySteps(human, steps)
-      })
-    })
-  },
-
-  drawRoute: function(geoArray, dirLayer) {
-    var arryLength = geoArray.length
-    var geoJSON = {
-      "type": "LineString",
-      "coordinates": []
-    }
-    for (var i=0;i<arryLength;i++) {
-      geoJSON.coordinates[i] = geoArray[i]
-    }
-    dirLayer.setGeoJSON(geoJSON)
-  },
-
-  addMarkers: function(type) {
-    var symbol, color, venues
-    if (type === "coffee") {
-      symbol = "cafe"
-      color = "#3fbfbf"
-      venues = this.coffee
-    } else if (type === "food") {
-      symbol = "restaurant"
-      color = "#2d7ac7"
-      venues = this.food
-    } else if (type === "drinks") {
-      symbol = "bar"
-      color = "#2dc787"
-      venues = this.drinks
-    }
-    var geoLocations = []
-    venues.forEach(function(place) {
-      var geoJSON = {
-        "type": "Feature",
-        "geometry": {
-          "type": "Point",
-          "coordinates": [place.venue.location.lng,place.venue.location.lat]
-        },
-        "properties": {
-          "title": place.venue.name,
-          "description": Map.urlChecker(place),
-          "marker-color": color,
-          "marker-size": "large",
-          "marker-symbol": symbol
-        }
-      }
-      geoLocations.push(geoJSON)
-    })
-    return geoLocations
-  },
-
-  urlChecker: function(place) {
-    if (place.venue.url !== undefined) {
-      return (place.venue.location.address + "<br>" + "<a href='" + place.venue.url + "'>" + "Website" + "</a>")
-    } else {
-      return (place.venue.location.address)
-    }
   },
 
   fetchVenues: function(type, callback) {
@@ -190,19 +135,118 @@ var Map = {
     })
   },
 
-  updateMap: function() {
-    this.startingPoints = []
-    var address1 = document.getElementById('address1').value
-    var address2 = document.getElementById('address2').value
-    this.findLocation(address1, App.user.email[1])
-    this.findLocation(address2, App.user.email[2])
+  populateTheMiddle: function() {
+    if (this.initLoad === true) {
+      var geoLocations = []
+      this.locationTypes.forEach(function(type) {
+        geoLocations.push(Map.addMarkers(type))
+      })
+      var merged = []
+      merged = merged.concat.apply(merged, geoLocations)
+      this.venueLayer.setGeoJSON(merged)
+      this.bindDirectionsEvent(this.userOneDirectionsLayer, 0, Map.venueLayer)
+      this.bindDirectionsEvent(this.userTwoDirectionsLayer, 1, Map.venueLayer)
+      this.bindVenueInfo(Map.venueLayer)
+      View.showVenueButtons()
+    }
+  },
+
+  bindVenueInfo: function(layer) {
+    layer.on('click', function(e) {
+      View.showVenueInfo()
+      var venue = Map.venueInfo[e.layer.feature.id]
+      View.renderVenueInfo(venue)
+    })
+  },
+
+  addMarkers: function(type) {
+    var symbol, color, venues
+    if (type === "coffee") {
+      symbol = "cafe"
+      color = "#3fbfbf"
+      venues = this.coffee
+    } else if (type === "food") {
+      symbol = "restaurant"
+      color = "#2d7ac7"
+      venues = this.food
+    } else if (type === "drinks") {
+      symbol = "bar"
+      color = "#2dc787"
+      venues = this.drinks
+    }
+    var geoLocations = []
+    venues.forEach(function(place) {
+      var geoJSON = {
+        "type": "Feature",
+        "id": place.venue.id,
+        "geometry": {
+          "type": "Point",
+          "coordinates": [place.venue.location.lng,place.venue.location.lat]
+        },
+        "properties": {
+          "title": place.venue.name,
+          "marker-color": color,
+          "marker-size": "large",
+          "marker-symbol": symbol
+        }
+      }
+      geoLocations.push(geoJSON)
+      Map.venueInfo[place.venue.id] = place.venue
+    })
+    return geoLocations
+  },
+
+  bindDirectionsEvent: function(dirLayer, human, layer) {
+    layer.on('click', function(e) {
+      var points = {
+        pointOne: Map.startingPoints[human].location,
+        pointTwo: e.layer.feature.geometry.coordinates
+      }
+      $.ajax({
+        type: 'POST',
+        url: '/directions',
+        data: points,
+        dataType: 'json'
+      }).success(function(data) {
+        var geoArray = data.routes[0].geometry.coordinates
+        Map.drawRoute(geoArray, dirLayer)
+
+        var steps = data.routes[0].steps
+        View.displaySteps(human, steps)
+      })
+    })
+  },
+
+  drawRoute: function(geoArray, dirLayer) {
+    var arryLength = geoArray.length
+    var geoJSONObject = {
+      "type": "LineString",
+      "coordinates": []
+    }
+    for (var i=0;i<arryLength;i++) {
+      geoJSONObject.coordinates[i] = geoArray[i]
+    }
+    var color
+    if (dirLayer._leaflet_id === 31) {
+      color = "#16a085"
+    } else {
+      color = "#c0392b"
+    }
+    var stylezzz = {
+      color: color,
+      weight: 4,
+      opacity: 0.8
+    }
+
+    dirLayer.setGeoJSON(geoJSONObject)
+    dirLayer.setStyle(stylezzz)
   },
 
   findTheMiddle: function() {
     var pythagorean = Math.pow((this.startingPoints[0].location[0] - this.startingPoints[1].location[0]),2) + Math.pow((this.startingPoints[0].location[1] - this.startingPoints[1].location[1]),2)
     return {
-      lat: (this.startingPoints[0].location[0] + this.startingPoints[1].location[0])/2,
-      lng: (this.startingPoints[0].location[1] + this.startingPoints[1].location[1])/2,
+      lat: (this.startingPoints[0].location[1] + this.startingPoints[1].location[1])/2,
+      lng: (this.startingPoints[0].location[0] + this.startingPoints[1].location[0])/2,
       radius: (Math.sqrt(pythagorean) * 10000)
     }
   }
--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', routes.middle);
 app.get('/middle', routes.middle);
 app.post('/places', routes.places)
 app.post('/directions', routes.directions)
